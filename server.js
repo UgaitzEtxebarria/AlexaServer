40a0432373201a5cb50b9cba578cfd19a0342684
UgaitzEtxebarria <etxeba17@gmail.com>
2019-11-06 20:08:33 +0100

    created first test

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 'use strict';
 
-var AlexaAppServer = require( 'alexa-app-server' );
+/*var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
+	appdir = "apps",
 	httpsEnabled: false,
 	port: process.env.PORT || 80
 } );
 
-server.start();
+server.start();*/
+
+var AlexaAppServer = require('alexa-app-server');
+ 
+var instance = AlexaAppServer.start({
+  server_root: __dirname,     // Path to root
+  public_html: "public_html", // Static content
+  app_dir: "apps",            // Location of alexa-app modules
+  app_root: "/alexa/",        // Service root
+  port: 8080                  // Port to use
+});
+ 
+//instance.stop();              // Stop the server
